afc4899c2572517b6a3299a6e349ed1911deeb63
Bill Wolf <wmwolf@asu.edu>
2021-01-15 16:52:06 -0600

    Update memebers with new leadership scheme.

--- _bin/generate_members_code.rb
+++ _bin/generate_members_code.rb
@@ -15,8 +15,11 @@ puts "Done!"
 
 # puts table
 
-acceptable_positions = ['Co-Chair', 'Red List Authority Coordinator',
+acceptable_positions = ['Co-Chair', 'Deputy Chair', 'Advisory Committee Member',
+                        'Red List Authority Coordinator',
                         'Programme Officer', 'Commission Member']
+categories = ['Leadership', 'Red List Authority Coordinator',
+              'Programme Officer', 'Commission Members']
 
 table.each do |member|
   unless acceptable_positions.include? member[:position]
@@ -32,7 +35,31 @@ table_es.each do |member|
   end
 end
 
-def format_li(member, img_position: :left, indent: '', text_color: nil, lang: :en)
+def format_li(member, img_position: :left, indent: '', text_color: nil,
+              lang: :en, display_role: false)
+
+  # maps roles to roles (does nothing in English)
+  position_dictionary = case lang
+  when :es
+   {
+      'Co-Chair' => 'Copresidenta',
+      'Deputy Chair' => 'Vicepresidenta',
+      'Advisory Committee Member' => 'Miembro del Comité Asesor',
+      'Red List Authority Coordinator' => 'Coordinador de la Autoridad de la Lista Roja',
+      'Programme Officer' => 'Oficial de Programa',
+      'Commission Member' => 'Miembro de la Comisión'
+    }
+  else
+   {
+      'Co-Chair' => 'Co-Chair',
+      'Deputy Chair' => 'Deputy Chair',
+      'Advisory Committee Member' => 'Advisory Committee Member',
+      'Red List Authority Coordinator' => 'Red List Authority Coordinator',
+      'Programme Officer' => 'Programme Officer',
+      'Commission Member' => 'Commission Member'
+    }
+  end
+
   res = indent + '<li class="media my-4">' + "\n"
   indent += '  '
   img_file = member[:image] || 'avatar-placeholder-sm.png'
@@ -48,7 +75,10 @@ def format_li(member, img_position: :left, indent: '', text_color: nil, lang: :e
   # puts "keys are #{member.keys}"
   # puts "last is #{member.values[0]} and first is #{member[:first]}"
   # puts "values are #{member.values}"
-  res += (indent + '<h5 class="mt-0' + (text_color.nil? ? '' : " #{text_color}") + '">' + member[:first] + ' ' + member.values[0] + '</h5>' + "\n")
+  res += (indent + '<h5 class="mt-0' + (text_color.nil? ? '' : " #{text_color}") + '">' + member[:first] + ' ' + member.values[0])
+  # optionally include position
+  res += ", #{position_dictionary[member[:position]]}" if display_role
+  res += ('</h5>' + "\n")
   res += (indent + "<p class='mb-1'>")
   res += "#{member[:description]}" || ''
   if member[:description] and member[:institution]
@@ -81,26 +111,42 @@ def format_li(member, img_position: :left, indent: '', text_color: nil, lang: :e
   res += (indent + '</li>' + "\n")
 end
 
-def format_list(table, position, img_position: :left, indent: '', bg_class: nil, lang: :en)
-  position_dictionary = case lang
+def format_list(table, category, img_position: :left, indent: '', bg_class: nil, lang: :en)
+  # maps categories to their proper translation (english does nothing)
+  categories_dictionary = case lang
   when :es
-   {
-      'Co-Chair' => 'Copresidentas',
-      'Red List Authority Coordinator' => 'Autoridad de la Lista Roja',
-      'Programme Officer' => 'Oficial de Programas',
-      'Commission Member' => 'Miembro de la Comisión'
+    {
+      'Leadership' => 'Liderazgo',
+      'Red List Authority Coordinator' => 'Cordinador de la Autoridad de la Lista Roja',
+      'Programme Officer' => 'Oficial de Programa',
+      'Commission Members' => 'Miembros de la Comisión'
     }
   else
-   {
-      'Co-Chair' => 'Co-Chairs',
+    {
+      'Leadership' => 'Leadership',
       'Red List Authority Coordinator' => 'Red List Authority Coordinator',
       'Programme Officer' => 'Programme Officer',
-      'Commission Member' => 'Commission Members'
+      'Commission Members' => 'Commission Members'
     }
   end
-  members = table.select { |member| member[:position] == position }
-  members.sort! { |m1, m2| m1[:last] <=> m2[:last] }
-  return unless members.count > 0
+
+  # maps categories of roles to specific roles in English always
+  categories_to_roles = {
+    'Leadership' => ['Co-Chair', 'Deputy Chair', 'Advisory Committee Member'],
+    'Red List Authority Coordinator' => ['Red List Authority Coordinator'],
+    'Programme Officer' => ['Programme Officer'],
+    'Commission Members' => ['Commission Member']
+  }
+
+  members = table.select do |member|
+    categories_to_roles[category].include? member[:position].strip
+  end
+  puts "There are #{members.count} members in category #{category}"
+  members.sort! { |m1, m2| [categories_to_roles[category].index(m1[:position]), m1[:last]] <=> [categories_to_roles[category].index(m2[:position]), m2[:last]] }
+  unless members.count > 0
+    puts "No memebers with category #{category}, which is positions #{categories_to_roles[category]}"
+    return ''
+  end
   res = ''
   # end previous container. Might have been main body, might be previous list
   res += (indent + "</div>\n")
@@ -113,13 +159,13 @@ def format_list(table, position, img_position: :left, indent: '', bg_class: nil,
   res += (indent + "<div class='col'>\n")
   indent += '  '
   res += (indent + "<h1 class='" + (bg_class ? 'text-white' : 'text-success') +
-    "'>#{position_dictionary[position]}")
+    "'>#{categories_dictionary[category]}")
   res += "</h1>\n"
   res += (indent + "<ul class='list-unstyled'>\n")
   indent += '  '
   text_color = bg_class ? 'text-white' : nil
   members.each do |member|
-    res += format_li(member, img_position: img_position, indent: indent.dup, text_color: text_color, lang: lang)
+    res += format_li(member, img_position: img_position, indent: indent.dup, text_color: text_color, lang: lang, display_role: category == 'Leadership')
   end
   indent = indent[(0...-2)]
   res += (indent + "</ul>\n")
@@ -133,21 +179,21 @@ def format_list(table, position, img_position: :left, indent: '', bg_class: nil,
   res
 end
 
-def format_all(table, positions, indent: '', lang: :en)
+def format_all(table, categories, indent: '', lang: :en)
   img_positions = [:left, :right]
   bg_classes = ['bg-light-green', 'bg-orange', 'bg-yellow', 'bg-dark-green']
   res = ''
-  positions.each_with_index do |position, i|
-    res += format_list(table, position, img_position: img_positions[i.modulo(img_positions.length)], indent: indent, bg_class: bg_classes[i], lang: lang)
+  categories.each_with_index do |category, i|
+    res += format_list(table, category, img_position: img_positions[i.modulo(img_positions.length)], indent: indent, bg_class: bg_classes[i], lang: lang)
   end
   res += (indent + '</div>' + "\n")
   res
 end
 
 File.open(File.join(bin_path, '..', '_includes', 'members_en.html'), 'w') do |f|
-  f.write(format_all(table, acceptable_positions, indent: ''))
+  f.write(format_all(table, categories, indent: ''))
 end
 
 File.open(File.join(bin_path, '..', '_includes', 'members_es.html'), 'w') do |f|
-  f.write(format_all(table_es, acceptable_positions, indent: '', lang: :es))
+  f.write(format_all(table_es, categories, indent: '', lang: :es))
 end
